51258da7c76c07a2bef37d9143fbab80ccb295f4
Nitin <nitin8285824551@gmail.com>
2025-01-18 10:08:19 +0530

    add created_at updated_at in all tables

--- center/models.py
+++ center/models.py
@@ -1,17 +1,22 @@
 from django.db import models
 from user.models import BaseUser
+from django.utils.timezone import now
 
 
 class Center(models.Model):
     name = models.CharField(max_length=255)
     location = models.TextField()
     contact_number = models.CharField(max_length=15)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
 
 class ClassName(models.Model):
     name = models.CharField(max_length=100, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
@@ -19,12 +24,16 @@ class ClassName(models.Model):
 
 class Section(models.Model):
     name = models.CharField(max_length=50, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     def __str__(self):
         return self.name
 
 
 class Subject(models.Model):
     name = models.CharField(max_length=100, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
@@ -34,6 +43,8 @@ class Batch(models.Model):
     class_name = models.ForeignKey(ClassName, on_delete=models.CASCADE, related_name="batch")
     section = models.ForeignKey(Section, on_delete=models.CASCADE, related_name="batch")
     subject  = models.ForeignKey(Subject, on_delete=models.CASCADE, related_name="batch")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         unique_together = ("class_name", "section", "subject")
@@ -50,6 +61,8 @@ class Student(models.Model):
     user = models.OneToOneField(BaseUser, on_delete=models.CASCADE, related_name="student")
     batches = models.ManyToManyField('Batch', related_name='student')
     center = models.ForeignKey(Center, on_delete=models.CASCADE, related_name="student")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         full_name = f"{self.user.first_name} {self.user.last_name}".strip()
@@ -59,7 +72,8 @@ class Student(models.Model):
 class Test(models.Model):
     name = models.CharField(max_length=255)
     batch = models.ForeignKey(Batch, on_delete=models.CASCADE, related_name="test")
-    date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.name} -  {self.batch.__str__()}"
@@ -73,13 +87,16 @@ class TestQuestion(models.Model):
     chapter_no = models.IntegerField()
     chapter_name = models.CharField(max_length=255)
     max_marks = models.PositiveIntegerField()
-
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"Q{self.question_number} - {self.chapter_name}"
 
 class Remark(models.Model):
     name = models.CharField(max_length=255, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
@@ -90,6 +107,8 @@ class QuestionResponse(models.Model):
     test = models.ForeignKey(Test, on_delete=models.CASCADE, related_name='response')
     marks_obtained = models.IntegerField()
     remark = models.ManyToManyField('Remark', related_name='remark')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         unique_together = ('question', 'student')  # Ensures a student can only respond once per question
@@ -102,11 +121,12 @@ class QuestionResponse(models.Model):
 class Attendance(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name="attendance")
     batch = models.ForeignKey(Batch, on_delete=models.CASCADE, related_name="attendance")
-    date = models.DateField()
     is_present = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-    class Meta:
-        unique_together = ('student', 'batch', 'date')
+    # class Meta:
+    #     unique_together = ('student', 'batch', 'created_at')
 
     def __str__(self):
         return f"{self.student} - {self.date}: {'Present' if self.is_present else 'Absent'}"
@@ -120,8 +140,9 @@ class Homework(models.Model):
 
     student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name="homework")
     batch = models.ForeignKey(Batch, on_delete=models.CASCADE, related_name="homework")
-    date = models.DateField()
     status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"Homework for {self.student} - {self.status}"
