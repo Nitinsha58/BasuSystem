06d7152eea90169deada60116ed98fac2e74038d
Nitin <nitin8285824551@gmail.com>
2025-02-05 16:55:38 +0530

    add both create user and add existing user in admissioncounsellor

--- inquiry_followup/forms.py
+++ inquiry_followup/forms.py
@@ -2,45 +2,62 @@ from user.models import BaseUser
 from .models import AdmissionCounselor, FollowUpStatus
 from django import forms
 
-
 class AdmissionCounselorForm(forms.ModelForm):
-    # Adding fields from BaseUser into the AdmissionCounselor form
-    first_name = forms.CharField(max_length=255)
-    last_name = forms.CharField(max_length=255)
-    phone = forms.CharField(max_length=15)
+    user = forms.ModelChoiceField(queryset=BaseUser.objects.all(), required=False)  # Optional user field
+    first_name = forms.CharField(max_length=255, required=False)
+    last_name = forms.CharField(max_length=255, required=False)
+    phone = forms.CharField(max_length=15, required=False)
 
     class Meta:
         model = AdmissionCounselor
-        fields = ['first_name', 'last_name', 'phone', 'center']
+        fields = ['user', 'first_name', 'last_name', 'phone', 'center']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        
-        # Prepopulate fields if updating an existing instance
+
+        # If updating an existing instance, populate fields
         if self.instance and self.instance.pk:
+            self.fields['user'].initial = self.instance.user
             self.fields['first_name'].initial = self.instance.user.first_name
             self.fields['last_name'].initial = self.instance.user.last_name
             self.fields['phone'].initial = self.instance.user.phone
 
-    def save(self, commit=True):
-        # If editing an existing AdmissionCounselor, update the associated BaseUser
-        if self.instance and self.instance.pk:
-            base_user = self.instance.user
-            base_user.first_name = self.cleaned_data['first_name']
-            base_user.last_name = self.cleaned_data['last_name']
-            base_user.phone = self.cleaned_data['phone']
-            base_user.save()
-        else:
-            # If creating a new AdmissionCounselor, create a new BaseUser first
-            base_user = BaseUser.objects.create(
-                first_name=self.cleaned_data['first_name'],
-                last_name=self.cleaned_data['last_name'],
-                phone=self.cleaned_data['phone'],
-            )
+    def clean(self):
+        cleaned_data = super().clean()
+        user = cleaned_data.get('user')
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
+        phone = cleaned_data.get('phone')
 
-        # Save the AdmissionCounselor instance and associate it with the BaseUser
-        admission_counselor = super().save(commit=False)
-        admission_counselor.user = base_user
+        # If no user is provided, first_name, last_name, and phone are required
+        if not user:
+            if not first_name:
+                self.add_error('first_name', "This field is required when no user is selected.")
+            if not last_name:
+                self.add_error('last_name', "This field is required when no user is selected.")
+            if not phone:
+                self.add_error('phone', "This field is required when no user is selected.")
+
+        return cleaned_data
+
+    def save(self, commit=True):
+        cleaned_data = self.cleaned_data
+        user = cleaned_data.get('user')
+
+        if user:
+            # If a user is provided, use it directly
+            admission_counselor = super().save(commit=False)
+            admission_counselor.user = user
+        else:
+            # Create a new BaseUser if user is not provided
+            base_user = BaseUser.objects.create(
+                first_name=cleaned_data['first_name'],
+                last_name=cleaned_data['last_name'],
+                phone=cleaned_data['phone'],
+            )
+            admission_counselor = super().save(commit=False)
+            admission_counselor.user = base_user
+
         if commit:
             admission_counselor.save()
         return admission_counselor
