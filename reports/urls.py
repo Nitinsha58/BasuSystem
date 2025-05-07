ce94f2d310d0f43eb463eb450231b2d0bcef1013
Nitin <nitin8285824551@gmail.com>
2025-05-07 15:43:05 +0530

    add student personal report in dashboard

--- reports/urls.py
+++ reports/urls.py
@@ -3,10 +3,12 @@ from .views import (
     student_report,
     batchwise_students,
     mentor_students,
+    student_personal_report
 )
 
 urlpatterns = [
     path('student/<uuid:stu_id>/', student_report, name='student_report'),
+    path('personal_report/<uuid:stu_id>/', student_personal_report , name='student_personal_report'),
     path('batchwise_students/', batchwise_students, name='batchwise_students'),
     path('mentor_students/', mentor_students, name='mentor_students'),
 ]
--- reports/views.py
+++ reports/views.py
@@ -108,6 +108,84 @@ def student_report(request, stu_id):
         'batches': batches,
     })
 
+@login_required(login_url='login')
+def student_personal_report(request, stu_id):
+    student = Student.objects.filter(stu_id=stu_id).first()
+    if stu_id and not student:
+        messages.error(request, "Invalid Student")
+        return redirect('staff_dashboard')
+
+    start_date_str = request.GET.get('start_date')
+    end_date_str = request.GET.get('end_date')
+
+    if start_date_str and end_date_str:
+        try:
+            start_date = datetime.strptime(start_date_str, "%Y-%m-%d").date()
+            end_date = datetime.strptime(end_date_str, "%Y-%m-%d").date()
+        except ValueError:
+            messages.error(request, "Invalid date format")
+            return redirect('student_personal_report', stu_id=stu_id)
+    else:
+        today = date.today()
+        start_date = today.replace(day=1)
+        end_date = today
+    
+    batches = Batch.objects.filter(class_name=student.class_enrolled).order_by('-created_at')
+    batch_wise_tests = {}
+
+    for batch in batches:
+        tests = Test.objects.filter(batch=batch).order_by('-date')
+        test_reports = []
+
+        for test in tests:
+            test_chapters = get_chapters_from_questions(test)
+            responses = QuestionResponse.objects.filter(test=test, student=student).select_related('question', 'remark')
+
+            chapter_remarks = calculate_testwise_remarks(responses, test_chapters)
+            marks_data = calculate_marks(responses, test_chapters)
+
+            test_reports.append({
+                'test': test,
+                'chapters': test_chapters,
+                'marks_total': marks_data['total'],
+                'marks_deducated': marks_data['deducted'],
+                'marks_obtained': marks_data['obtained'],
+                'remarks': marks_data['remarks'],
+                'max_marks': marks_data['max_marks'],
+                'marks': {
+                    'percentage': marks_data['percentage'],
+                    'obtained_marks': marks_data['obtained_total'],
+                    'max_marks': marks_data['total_max'],
+                },
+                'chapter_wise_test_remarks': chapter_remarks,
+            })
+
+        batch_wise_tests[batch] = test_reports
+
+    combined_attendance = get_combined_attendance(student, start_date, end_date)
+    batchwise_attendance = get_batchwise_attendance(student,start_date, end_date)
+    combined_homework = get_combined_homework(student, start_date, end_date)
+    batchwise_homework = get_batchwise_homework(student, start_date, end_date)
+    calendar_data = get_monthly_calendar(student)
+
+    return render(request, 'reports/student_report.html', {
+        'student': student,
+        'combined_attendance': combined_attendance,
+        'batchwise_attendance': batchwise_attendance,
+        'combined_homework': combined_homework,
+        'batchwise_homework': batchwise_homework,
+        'current_month': calendar_data['calendar'],
+        'current_month_name': calendar_data['month_name'],
+        'current_month_present_count': calendar_data['present_count'],
+        'current_month_total_count': calendar_data['present_count'] + calendar_data['absent_count'],
+        'current_month_percentage': calendar_data['percentage'],
+        'start_date': start_date,
+        'end_date': end_date,
+
+        'batch_wise_tests': batch_wise_tests,
+        'batches': batches,
+    })
+
 
 @login_required(login_url='login')
 def batchwise_students(request):
@@ -155,8 +233,6 @@ def mentor_students(request):
         'class_mentorships': class_mentorships,
     })
 
-@login_required(login_url='login')
-def chapterwise_student_report(request, student_id):
     batches = Batch.objects.all()
     batch = None
     students = None
